70b7a3f7f4379d72a68db4c2802587ec6e8f594b
Dhruv Pankhaniya <159551134+dhruvpankhaniya@users.noreply.github.com>
2024-09-14 13:14:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateform(){
+    var Sname = document.getElementById("sname").value;
+    var cname = document.getElementById("cname").value;
+    var dname = document.getElementById("dname").value;
+    var DOB = document.getElementById("DOB").value;
+    var no = document.getElementById("no").value;
+    var cno = document.getElementById("cno").value;
+
+    console.log(Sname,cname,dname,DOB,no,cno); ;
+}
